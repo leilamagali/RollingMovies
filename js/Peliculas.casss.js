86b57294995466c328552b5e4c0f7f354f63968c
aramayowil <aramayowil@gmail.com>
2024-03-11 23:29:38 -0300

    peliculasDB teminanda

---
+++ js/Peliculas.casss.js
@@ -0,0 +1,19 @@
+export class Peliculas {
+
+    constructor(codigo, nombre, categoria, genero, publicado, img_portada, img_fondo ,url_trailer, url_video){
+        this.codigo;
+        this.nombre;
+        this.categoria
+        this.genero;
+        this.publicado;
+        this.img_portada
+        this.img_fondo
+        this.url_trailer;
+        this.url_video;
+    }
+
+    mostrar(){
+        
+    }
+
+}
---
+++ js/PeliculasDB.class.js
@@ -0,0 +1,70 @@
+export class PeliculasDB {
+    constructor(){
+    }
+
+    establecerConexion(){  //devuelve los datos de la tabla Peliculas, sino hay ninguna la crea
+        let peliculasDB;
+
+        const tablaPeliculas = JSON.parse(localStorage.getItem('peliculas'));
+    
+        if (tablaPeliculas) {
+            peliculasDB = tablaPeliculas;
+    
+        } else {
+            //en caso de no encontrar ninguna base de datos pelicula creamos una
+            const nuevaPelicula = [
+                {   codigo: 0,
+                    nombre: 'Oppenheimer',
+                    categoria: 'pelicula',
+                    genero: 'drama',
+                    publicado: true,
+                    img_portada: '/img/peliculas/portadas/Oppenheimer_portada.jpg',
+                    img_fondo: '/img/peliculas/fondos/Oppenheimer_fondo.jpg',
+                    url_trailer: 'https://youtu.be/yLYbOe914ZU?si=s86bPU_fgA0AjQi1',
+                    url_pelicula: 'https://youtu.be/Krlj0cw42Do?si=8vZDtsJwRp7Q6uDF'
+                }
+            ];
+    
+            localStorage.setItem('peliculas', JSON.stringify(nuevaPelicula));
+            peliculasDB =JSON.parse(localStorage.getItem('peliculas'));
+        }
+    
+        return peliculasDB;    
+    }
+
+    obtenerCodigo(){ //obtiene el siguiente codigo segun la base de datos de peliculas
+        const datos = this.establecerConexion();
+        const ultimoElemento = datos.length - 1;
+        return datos[ultimoElemento].codigo + 1;
+    }
+
+    agregarPelicula(objPelicula){ 
+
+        const nuevaPelicula = {
+            codigo: objPelicula.codigo,
+            nombre: objPelicula.nombre,
+            categoria: objPelicula.categoria,
+            genero: objPelicula.genero,
+            publicado: objPelicula.publicado,
+            img_portada: objPelicula.img_portada,
+            img_fondo: objPelicula.img_fondo,
+            url_trailer: objPelicula.url_trailer,
+            url_pelicula: objPelicula.url_pelicula
+        };
+
+        let peliculasDB = this.establecerConexion();
+ 
+        peliculasDB.push(nuevaPelicula);
+        //Actualizo la nueva base de datos
+        localStorage.setItem('peliculas', JSON.stringify(peliculasDB));
+    }
+
+
+    buscarPelicula(){
+
+    }
+
+    mostrarPeliculas(){
+
+    }
+}
